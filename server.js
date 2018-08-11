80757cb0ad709eb4857bf63cd3be998f92460a30
karkran <nikhil.karkra@anz.com>
2018-08-12 00:31:28 +1000

    server settings

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ server.get('/', (req, res) => {
 
 server.use(express.static('build'));
 
+
 const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`App listening on port ${port}`);
